4986464b01216895066a494cf0b6c4e0107c5f32
Hamburger <72227790+bwrbur@users.noreply.github.com>
2023-11-13 23:52:44 +0000

    Update and rename perms.md to perms.sql

--- perms.sql
+++ perms.sql
@@ -1,2 +1,2 @@
 CREATE USER 'admin'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
-GRANT ALL PRIVILEGES ON <db>_prod.* TO 'admin'@'localhost';
+GRANT ALL PRIVILEGES ON <db_name>.* TO 'admin'@'localhost';
