1a236bc97059eaf4db797e3ef545be6311a3a52f
Hamburger <72227790+bwrbur@users.noreply.github.com>
2023-11-25 17:30:27 +0000

    Update perms.sql

--- perms.sql
+++ perms.sql
@@ -1,2 +1,2 @@
 CREATE USER '<user>'@'localhost' IDENTIFIED WITH mysql_native_password BY '<password>';
-GRANT ALL PRIVILEGES ON db_name.* TO '<user>'@'localhost';
+GRANT ALL PRIVILEGES ON <database name>.* TO '<user>'@'localhost';
